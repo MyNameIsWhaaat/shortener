a9d9129244329cfa9c337ca4d653384c914e4305
Ekaterina Perminova <katanatroll@yandex.ru>
2026-03-28 00:59:32 +0400

    refactor(logger): migrate logger wrapper to wbf zlog

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,34 +1,65 @@
 package logger
 
 import (
-	"log/slog"
-	"os"
+	"github.com/wb-go/wbf/zlog"
 )
 
-var Log *slog.Logger
-
 func Init() {
-	opts := slog.HandlerOptions{
-		Level: slog.LevelInfo,
-	}
-
-	handler := slog.NewJSONHandler(os.Stdout, &opts)
-	Log = slog.New(handler)
-	slog.SetDefault(Log)
+	zlog.InitConsole()
 }
 
 func Info(msg string, args ...any) {
-	Log.Info(msg, args...)
+	event := zlog.Logger.Info()
+
+	for i := 0; i+1 < len(args); i += 2 {
+		key, ok := args[i].(string)
+		if !ok {
+			continue
+		}
+		event = event.Interface(key, args[i+1])
+	}
+
+	event.Msg(msg)
 }
 
 func Error(msg string, args ...any) {
-	Log.Error(msg, args...)
+	event := zlog.Logger.Error()
+
+	for i := 0; i+1 < len(args); i += 2 {
+		key, ok := args[i].(string)
+		if !ok {
+			continue
+		}
+		event = event.Interface(key, args[i+1])
+	}
+
+	event.Msg(msg)
 }
 
 func Debug(msg string, args ...any) {
-	Log.Debug(msg, args...)
+	event := zlog.Logger.Debug()
+
+	for i := 0; i+1 < len(args); i += 2 {
+		key, ok := args[i].(string)
+		if !ok {
+			continue
+		}
+		event = event.Interface(key, args[i+1])
+	}
+
+	event.Msg(msg)
 }
 
 func Warn(msg string, args ...any) {
-	Log.Warn(msg, args...)
+	event := zlog.Logger.Warn()
+
+	for i := 0; i+1 < len(args); i += 2 {
+		key, ok := args[i].(string)
+		if !ok {
+			continue
+		}
+		event = event.Interface(key, args[i+1])
+	}
+
+	event.Msg(msg)
 }
